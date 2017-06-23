3e25bd30bfb0307f2102392bc62cab582feef4eb
Troy Center <troycenter1@gmail.com>
2017-06-23 14:05:15 -0700

    main login is working, update dashpage

--- apps/login/models.py
+++ apps/login/models.py
@@ -1,6 +1,65 @@
 # -*- coding: utf-8 -*-
+#pylint: disable=c0111
 from __future__ import unicode_literals
-
+import re
+import bcrypt
 from django.db import models
 
 # Create your models here.
+class UserManager(models.Manager):
+    def registervalidate(self, postdata):
+        results = {'status':True, 'errors':[], 'user':None}
+        if not postdata['first_name'] or len(postdata['first_name']) < 3:
+            results['errors'].append("First name must be at least 3 characters")
+            results['status'] = False
+        if not postdata['last_name'] or len(postdata['last_name']) < 3:
+            results['errors'].append("Last name must be at least 3 characters")
+            results['status'] = False
+        if not postdata['email'] or \
+            not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", postdata['email']):
+            results['errors'].append("Email must be a valid email address")
+            results['status'] = False
+        if not postdata['password'] or len(postdata['password']) < 8:
+            results['errors'].append("Password must be at least 8 characters")
+            results['status'] = False
+        if postdata['password'] != postdata['cpassword']:
+            results['errors'].append("Passwords do not match")
+            results['status'] = False
+        user = User.objects.filter(email=postdata['email'])
+        if user:
+            results['status'] = False
+            results['errors'].append("User already exists. Please login...")
+        if results['status']:
+            password = bcrypt.hashpw(postdata['password'].encode(), bcrypt.gensalt())
+            user = User.objects.create(
+                first_name=postdata['first_name'],
+                last_name=postdata['last_name'],
+                email=postdata['email'],
+                password=password)
+            results['user'] = user
+        return results
+
+    def loginvalidate(self, postdata):
+        results = {'status':True, 'errors':[], 'user':None}
+        user = User.objects.filter(email = postdata['email'])
+        print user[0]
+        # user = User.objects.filter(email=postdata['email'])
+        # userlen = len(user)
+        # if userlen > 1:
+        #     if user['password'] != \
+        #     bcrypt.hashpw(postdata['password'].encode(), user['password'].encode()):
+        #         results['status'] = False
+        # else:
+        #     results['user'] = user[0]    
+        return results
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.email
+    objects = UserManager()
--- apps/login/views.py
+++ apps/login/views.py
@@ -1,6 +1,37 @@
 # -*- coding: utf-8 -*-
+#pylint: disable=C0111
 from __future__ import unicode_literals
-
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import User
 # Create your views here.
+def index(request):
+    # User.objects.all().delete
+    return render(request, 'login/index.html')
+
+def register(request):
+    results = User.objects.registervalidate(request.POST)
+    print "***UserResult, back in views ***", newuser['status']
+    if not results['status']:
+        for error in results['errors']:
+            messages.error(request, error)
+        return redirect('/')
+    else:
+        messages.success(request, "User was created, please login.")
+    return redirect('/')
+
+def login(request):
+    results = User.objects.loginvalidate(request.POST)
+    if results['status'] is False:
+        messages.error(request, 'Please check your login and try again...')
+        return redirect('/')
+    # request.session['userid'] = results['user'].id
+    # request.session['first_name'] = results['user'].first_name
+    # request.session['last_name'] = results['user'].last_name
+    return redirect('/mainpage')
+
+def logout(request):
+    request.session.all().flush()
+
+def mainpage(request):
+    return render(request, 'login/mainpage.html')
